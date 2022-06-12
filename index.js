de30e0718659f061c517cc8e30f2e581a9a566f3
ndalilavin <vinson.steve@student.moringaschool.com>
2022-06-12 23:56:30 +0300

    commit tree 618979fd8cc71deb0238bf0cd2689a9ffbbd45f9

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(name, filter1) {
+    return name.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === filter1.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(name, testString) {
+    return name.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(name, filter1Name) {
+    return name.filter((record) => record.name === filter1Name);
+  }
